4a12c0f01844083233f5c26c2e14086436948c90
Vineetchelani1304 <vineetchelani545@gmail.com>
2024-07-06 14:48:09 +0530

    update side bar

--- Client/src/components/SideBar.jsx
+++ Client/src/components/SideBar.jsx
@@ -16,32 +16,20 @@ const SideBar = () => {
     };
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col text-center justify-center items-center">
             <div className="text-4xl font-bold text-blue-600 p-2">Expense Tracker</div>
-            <div className="flex flex-col px-4 mt-[10%] gap-4">
-                <div className="relative">
-                    <div
-                        className="hover:bg-blue-200 border-slate-300 border-[1px] bg-blue-300 font-bold text-center p-2 text-slate-800 rounded-lg cursor-pointer"
-                        onClick={toggleDropdown}
-                    >
-                        Menu
-                    </div>
-                    {dropdownOpen && (
-                        <div className="absolute right-0 mt-2 w-40 bg-white rounded-lg shadow-lg border border-slate-300">
-                            <div
-                                className="hover:bg-blue-200 border-slate-300 border-[1px] p-2 text-center text-slate-800 rounded-t-lg cursor-pointer"
-                                onClick={() => navigate('/')}
-                            >
-                                Dashboard
-                            </div>
-                            <div
-                                className="hover:bg-blue-200 border-slate-300 border-[1px] p-2 text-center text-slate-800 rounded-b-lg cursor-pointer"
-                                onClick={logout}
-                            >
-                                Logout
-                            </div>
-                        </div>
-                    )}
+            <div className="flex flex-col  mt-[10%] gap-4">
+                <div
+                    className="hover:bg-blue-200 border-slate-400 border-[1px] p-2 px-3 text-center text-slate-800 rounded-lg cursor-pointer"
+                    onClick={() => navigate('/')}
+                >
+                    Dashboard
+                </div>
+                <div
+                    className="hover:bg-blue-200 border-slate-400 border-[1px] p-2 text-center text-slate-800 rounded-lg cursor-pointer"
+                    onClick={logout}
+                >
+                    Logout
                 </div>
             </div>
         </div>
