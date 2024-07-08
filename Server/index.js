19d19f93aefb55dce733a08ccda1b9e19725faaa
Vineetchelani1304 <vineetchelani545@gmail.com>
2024-07-08 18:36:48 +0530

    format document

--- Server/index.js
+++ Server/index.js
@@ -13,27 +13,27 @@ const app = express();
 const PORT = 4000;
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
-app.listen(PORT,()=>{
-    console.log('listening on port',PORT);
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
 })
 
 app.use(express.json())
 
 DataBase.DbConnection();
 
-app.post('/signup',Signup);
-app.post('/login',Login);
-app.post('/createExpense',auth,createExpense);
-app.post('/createShare',auth,createShare);
-app.post('/createPersonal',auth,CreatePersonal);
+app.post('/signup', Signup);
+app.post('/login', Login);
+app.post('/createExpense', auth, createExpense);
+app.post('/createShare', auth, createShare);
+app.post('/createPersonal', auth, CreatePersonal);
 app.post('/settleExpense', auth, SettleExpense); // Change to POST
-app.get('/getUserExpenses',auth,getUserExpenses);
-app.get('/getSettlements',auth,GetAllSettlements);
-app.get('/expenses/:expenseId',auth, getExpenseDetails);
-app.delete('/deleteHistory',auth, deleteHistory);
-app.get('/',(req,res)=>{
+app.get('/getUserExpenses', auth, getUserExpenses);
+app.get('/getSettlements', auth, GetAllSettlements);
+app.get('/expenses/:expenseId', auth, getExpenseDetails);
+app.delete('/deleteHistory', auth, deleteHistory);
+app.get('/', (req, res) => {
     res.send("hey there")
 })
