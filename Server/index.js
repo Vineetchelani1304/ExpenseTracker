efdd720352d563a3e67c9a90464c5108fffdc902
Vineetchelani1304 <vineetchelani545@gmail.com>
2024-07-24 23:39:56 +0530

    fixing cors from backend

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = 4000;
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"https://expensetracker-rtqz.onrender.com"],
     credentials: true
 }));
 app.listen(PORT, () => {
