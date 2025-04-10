f75d38565ebddc46df50b0d4075a17dd88eb0f91
Vineet Chelani <vineetchelani545@gmail.com>
2025-04-10 20:08:30 +0530

    deployed on aws

--- Client/src/utils/backendUrl.js
+++ Client/src/utils/backendUrl.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = "http://ec2-13-233-28-145.ap-south-1.compute.amazonaws.com";
+const BACKEND_URL = "http://ec2-13-200-185-121.ap-south-1.compute.amazonaws.com";
 
 export default BACKEND_URL;
