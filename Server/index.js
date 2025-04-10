460735deb63afb52877c1d4bf1e7b95afb7fd8c9
Vineet Chelani <vineetchelani545@gmail.com>
2025-04-10 22:27:27 +0530

    add s3 bucket frontend url

--- Server/index.js
+++ Server/index.js
@@ -14,11 +14,16 @@ const app = express();
 const PORT = 4000;
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://viexpensetracker-vineet-chelanis-projects.vercel.app'],
+    origin: [
+        'http://localhost:5173',
+        'https://viexpensetracker-vineet-chelanis-projects.vercel.app',
+        'https://d3gbpcotpq0cgl.cloudfront.net' // ✅ Add this line
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
